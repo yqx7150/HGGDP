0214c6cc052ddd509ba823a3be2375d0a94eebed
Qiegen Liu <yqx7150@163.com>
2020-11-19 15:28:04 +0800

    Update __init__.py

--- hggdp_rec/hggdp/siat/__init__.py
+++ hggdp_rec/hggdp/siat/__init__.py
@@ -2,5 +2,8 @@
 from .siat_train import *
 #test code for SIAT
 from .siat_multich import *
+from .siat_multich_compare_modl import *
+from .siat_multich_compare_ddp import *
 from .siat_multinoise import *
+
 
